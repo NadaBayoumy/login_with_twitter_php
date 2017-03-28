6dfb9e9d0eeda8f5513a83f11e653fb0a7e3d486
NadaBayoumy <nada.bayoumy@hotmail.com>
2017-03-28 23:00:39 +0200

    login with twitter

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+define('CONSUMER_KEY', 'RZHIa1krnkI8gIwBW4rTkg58W');
+define('CONSUMER_SECRET', 'OlY1LmLEVxJr1KV4luDcEquxtQADWNyDhQV61bv78jHfILSgIb');
+define('OAUTH_CALLBACK', 'http://loginwithtwitter.com/process.php');
+?>
